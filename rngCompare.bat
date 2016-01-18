79c8b98a2ed7e1a4cdbe44fa5198516bad02ad7d
Ravkrat <yggdrasilresearch@gmail.com>
2016-01-17 21:33:31 -0600

    cleaning up debug echos

--- rngCompare.bat
+++ rngCompare.bat
@@ -3,7 +3,6 @@ goto :rngCompareInitVarSet
 
 ::initial variable set
 :rngCompareInitVarSet
-echo in rngCompare:rngCompareInitVarSet
 set MaxI=0
 set MaxII=0
 set MinI=0
@@ -14,7 +13,6 @@ goto :afterPass
 
 ::after rngInputTestPass
 :afterPass
-echo in rngCompare:afterPass
 set MinI=%~1
 set MinII=%~2
 set MaxI=%~3
@@ -24,21 +22,18 @@ goto :randomGen
 
 ::generate two random numbers
 :randomGen
-echo in rngCompare:randomGen
 set /a rngI=(%RANDOM%*%MaxI%/32768)+%MinI%
 set /a rngII=(%RANDOM%*%MaxII%/32768)+%MinII%
 goto :compareRNG
 
 ::add the two rng numbers
 :compareRNG
-echo in rngCompare:compareRNG
 ::set compare=0
 set /a compare=%rngI%+%rngII%
 goto :passToRNGInputTest
 
 ::pass the compare var back to rngInputTest
 :passToRNGInputTest
-echo in rngCompare:passToRNGInputTest
 call rngInputTest.bat %compare% 
 
 
--- rngInputTest.bat
+++ rngInputTest.bat
@@ -1,12 +1,9 @@
 @ECHO off
 ::increment the loop-check var
 set /a checkLoop+=1
-echo checkLoop=%checkLoop%
-pause
 goto :initLoopCheck
 
 :initLoopCheck
-echo in rngInputTest:initLoopCheck
 set logicI=1
 set logicII=2
 set logicIII=3
@@ -17,37 +14,30 @@ IF %checkLoop%==%logicIII% goto:EOF
 
 
 :rngInputTestInitVarSet
-echo in rngInputTest:rngInputTestInitVarSet
-set rngCaveI=0
-set rngCaveII=0
-set minCaveI=0
-set maxCaveI=50
-set minCaveII=0
-set maxCaveII=50
+set minCharCityStart=0
+set maxCharCityStart=50
+set minAlignmentCityStart=0
+set maxAlignmentCityStart=50
 goto :logic
 
 
 :logic
-echo in rngInputTest:logic
 IF %checkLoop%==%logicI% goto :rngInputTestPass
 IF %checkLoop%==%logicII% goto :rngInputTestDisplay
 IF %checkLoop%==%logicIII% goto :end
 ::goto :rngInputTestPass
 
 :rngInputTestPass
-echo in rngInputTest:rngInputeTestPass
 set checkLoop+=1
-call rngCompare.bat %minCaveI% %minCaveII% %maxCaveI% %maxCaveII%
+call rngCompare.bat %minCharCityStart% %minAlignmentCityStart% %maxCharCityStart% %maxAlignmentCityStart%
 goto :rngInputTestDisplay
 
 :rngInputTestDisplay
 IF %checkLoop%==%logicIII% goto :end
-echo in rngInputTest:rngInputTestDisplay
 set /a checkLoop+=1
 set result=%~1
-set displayResult=%result%
-echo checkLoop:%CheckLoop%
-echo result:%displayResult%
+::set displayResult=%result%
+echo result:%result%
 pause
 
 
