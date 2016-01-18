71aa5684a915f16bbd393e1b140730880e8ec7fb
Ravkrat <yggdrasilresearch@gmail.com>
2016-01-17 21:19:28 -0600

    finished testing
    finished making a rng

--- rngCompare.bat
+++ rngCompare.bat
@@ -3,15 +3,18 @@ goto :rngCompareInitVarSet
 
 ::initial variable set
 :rngCompareInitVarSet
+echo in rngCompare:rngCompareInitVarSet
 set MaxI=0
 set MaxII=0
 set MinI=0
 set MinII=0
 set result=0
+
 goto :afterPass
 
 ::after rngInputTestPass
 :afterPass
+echo in rngCompare:afterPass
 set MinI=%~1
 set MinII=%~2
 set MaxI=%~3
@@ -21,18 +24,22 @@ goto :randomGen
 
 ::generate two random numbers
 :randomGen
+echo in rngCompare:randomGen
 set /a rngI=(%RANDOM%*%MaxI%/32768)+%MinI%
 set /a rngII=(%RANDOM%*%MaxII%/32768)+%MinII%
 goto :compareRNG
 
-::compare the two random numbers
+::add the two rng numbers
 :compareRNG
-::set compare=
+echo in rngCompare:compareRNG
+::set compare=0
 set /a compare=%rngI%+%rngII%
 goto :passToRNGInputTest
 
-::pass the compare result back to rngInputTest
+::pass the compare var back to rngInputTest
 :passToRNGInputTest
+echo in rngCompare:passToRNGInputTest
 call rngInputTest.bat %compare% 
+
 
 
--- rngInputTest.bat
+++ rngInputTest.bat
@@ -1,40 +1,55 @@
 @ECHO off
-set /a checkLoop+=0
+::increment the loop-check var
+set /a checkLoop+=1
+echo checkLoop=%checkLoop%
+pause
 goto :initLoopCheck
 
 :initLoopCheck
+echo in rngInputTest:initLoopCheck
 set logicI=1
 set logicII=2
+set logicIII=3
 IF %checkLoop%==%logicI% goto :rngInputTestInitVarSet
 IF %checkLoop%==%logicII% goto :logic
+IF %checkLoop%==%logicIII% goto:EOF
+
 
 
 :rngInputTestInitVarSet
+echo in rngInputTest:rngInputTestInitVarSet
 set rngCaveI=0
 set rngCaveII=0
 set minCaveI=0
-set maxCaveI=100
+set maxCaveI=50
 set minCaveII=0
-set maxCaveII=100
+set maxCaveII=50
 goto :logic
 
 
+:logic
+echo in rngInputTest:logic
+IF %checkLoop%==%logicI% goto :rngInputTestPass
+IF %checkLoop%==%logicII% goto :rngInputTestDisplay
+IF %checkLoop%==%logicIII% goto :end
+::goto :rngInputTestPass
+
 :rngInputTestPass
-set checkLoop=2
+echo in rngInputTest:rngInputeTestPass
+set checkLoop+=1
 call rngCompare.bat %minCaveI% %minCaveII% %maxCaveI% %maxCaveII%
 goto :rngInputTestDisplay
 
-
-
-:logic
-IF %checkLoop%==%logicI% goto :rngInputTestPass
-IF %checkLoop%==%logicII% goto :rngInputTestDisplay
-goto :rngInputTestPass
-
 :rngInputTestDisplay
+IF %checkLoop%==%logicIII% goto :end
+echo in rngInputTest:rngInputTestDisplay
+set /a checkLoop+=1
 set result=%~1
-
-echo result:%result%
+set displayResult=%result%
+echo checkLoop:%CheckLoop%
+echo result:%displayResult%
 pause
-exit /b
 
+
+
+:end
